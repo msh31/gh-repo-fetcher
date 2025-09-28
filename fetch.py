947049b8ab903cbda24de2ba0079b0ce4198de9a
msh31 <mshendriks03@gmail.com>
2025-09-28 19:19:45 +0200

    feat: add proper tool installation checks
    - checks if github cli & sqlite3 are installed
    - nice lil func
    
    python ain't that bad, right now..

--- fetch.py
+++ fetch.py
@@ -1,11 +1,19 @@
 import json, sqlite3, subprocess
 
-output = subprocess.check_output(["gh", "version"])
+def check_if_installed(cmd):
+    try:
+        subprocess.run([cmd, "--version"])
+        return True
+    except (FileNotFoundError, subprocess.CalledProcessError):
+        return False
 
-if len(output) == 0:
-    print("the github cli is required for this script to work!")
-else:
-    print("goodjob! github cli is installed")
+if not check_if_installed("gh"):
+    print("GitHub CLI is not installed or not found in your PATH")
+
+if not check_if_installed("sqlite3"):
+    print("SQLite3 is not installed or not found in your PATH")
+
+print("\ngoodjob! all requirements are fulfilled. Let's continue")
 # print(output)
 #
 # connection = sqlite3.connect("projects.db")
